9e1cbad215b4533febeb55e2fe573c3fef0ef1be
Dawei Hao <dwsamurai84@gmail.com>
2024-11-25 08:12:45 +0800

    加上 註解。

--- main.py
+++ main.py
@@ -9,7 +9,17 @@ phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 print(phonetic_dict)
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+def generate_phonetic():
+    word = input("Enter a word: ").upper()
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+        
+    except KeyError as error:
+        print("Sorry, only letters in the alphabet please.")
+        print(error)
+        generate_phonetic() # 排除障礙之後，再call function一次。
+    
+    else:
+        print(output_list)
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+generate_phonetic()
